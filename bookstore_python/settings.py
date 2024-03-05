8b19c54f22113aeb1525129f03d3b501245b62e3
Cjr88 <carloscjr70@gmail.com>
2024-03-05 13:11:26 -0300

    acrescentando token e authorization

--- bookstore_python/settings.py
+++ bookstore_python/settings.py
@@ -135,7 +135,12 @@ INTERNAL_TIPS = [
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ]
 
 }
