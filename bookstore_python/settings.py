dca59466d0d50501c8e68979feaf973561afb64e
Cjr88 <carloscjr70@gmail.com>
2024-03-21 21:09:23 -0300

    adicionando logging

--- bookstore_python/settings.py
+++ bookstore_python/settings.py
@@ -1,4 +1,4 @@
-
+import logging
 import os
 from pathlib import Path
 
@@ -15,7 +15,7 @@ SECRET_KEY = "django-insecure-f*k@=53bc5!shef1-6w+m$-g)kspbaljz%8k4(j7iuc-u2_dyd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'carlospaiva88.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 
 # Application definition
@@ -145,3 +145,22 @@ INTERNAL_IPS = [
 ]
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
